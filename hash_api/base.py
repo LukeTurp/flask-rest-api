a7ad116a2430f182c75bae3f0398b8b30d5178dd
LukeTurp <luketurpin7@gmail.com>
2018-04-04 01:02:24 -0700

    Added Additional Token Settings

--- hash_api/base.py
+++ hash_api/base.py
@@ -2,6 +2,7 @@
 
 import logging
 
+
 from flask import Flask
 from flask import jsonify
 from flask_mongoengine import MongoEngine
@@ -29,6 +30,9 @@ app.config['MONGODB_PASSWORD'] = config.mongo_pass
 app.config['JWT_SECRET_KEY'] = config.jwt_secret
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=1)
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(hours=4)
+
 
 app.logger.addHandler(handler)
 
